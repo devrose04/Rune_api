3dfc4b09b0c372d21ed04433afb512065d4b5b02
devrose04 <webdevrose04@gmail.com>
2020-12-17 08:40:38 -0600

    use jest 'projects' to separate unit & integration tests

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  /**
-   * This will match ALL tests by default, both unit and integration,
-   * so take care to provide the path to the type you want to run, or
-   * just use the script in the package.json
-   */
-  testMatch: ['**/test/**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js'],
   transform: { '^.+\\.ts$': 'ts-jest' },
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json',
+      tsConfig: 'tsconfig.json',
     },
   },
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['**/test/unit/**/*.spec.ts'],
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['**/test/integration/**/*.spec.ts'],
+    },
+  ],
 };
